67990b47be546a2eebd97d6c3e42871473754a30
xujing0720 <l>
2019-02-14 20:11:40 +0800

    测试jenkins更新

--- src/main/java/com/xj/bean/User.java
+++ src/main/java/com/xj/bean/User.java
@@ -3,4 +3,28 @@ package com.xj.bean;
 public class User {
     private String name;
     private  String passward;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassward() {
+        return passward;
+    }
+
+    public void setPassward(String passward) {
+        this.passward = passward;
+    }
+
+    public User() {
+    }
+
+    public User(String name, String passward) {
+        this.name = name;
+        this.passward = passward;
+    }
 }
